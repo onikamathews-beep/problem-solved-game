441306d6759ac6480e28cbf1e5ccc3ea40a95e52
onikamathews-beep <onikamathews@gmail.com>
2026-07-23 16:13:16 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'problem-solved-v24';
+const CACHE_NAME = 'problem-solved-v25';
 const CORE_ASSETS = [
   './',
   './index.html',
   './principles-in-practice-logo.png',
-  './manifest.json?v=24',
-  './icon-192.png?v=24',
-  './icon-512.png?v=24'
+  './manifest.json?v=25',
+  './icon-192.png?v=25',
+  './icon-512.png?v=25'
 ];
 
 self.addEventListener('install', event => {
