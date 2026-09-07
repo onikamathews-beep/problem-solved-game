f0954a17940d504b6b89dbfc9d19e9463d9e4b2f
onikamathews-beep <onikamathews@gmail.com>
2026-09-07 09:49:07 -0700

    Bump Grow multiplayer cache

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'problem-solved-v30';
+const CACHE_NAME = 'problem-solved-v31';
 const CORE_ASSETS = [
   './',
   './index.html',
   './grow-where-youre-planted.html',
   './grow-decks.html',
   './principles-in-practice-logo.png',
-  './manifest.json?v=30',
-  './icon-192.png?v=30',
-  './icon-512.png?v=30'
+  './manifest.json?v=31',
+  './icon-192.png?v=31',
+  './icon-512.png?v=31'
 ];
 
 self.addEventListener('install', event => {
