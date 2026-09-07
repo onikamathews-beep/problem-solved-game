0a7a1ffd72e0b854dff1e211f110e4a83a9b58ce
onikamathews-beep <onikamathews@gmail.com>
2026-09-07 09:48:41 -0700

    Refresh cache after Grow lobby polish

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'problem-solved-v29';
+const CACHE_NAME = 'problem-solved-v30';
 const CORE_ASSETS = [
   './',
   './index.html',
   './grow-where-youre-planted.html',
   './grow-decks.html',
   './principles-in-practice-logo.png',
-  './manifest.json?v=29',
-  './icon-192.png?v=29',
-  './icon-512.png?v=29'
+  './manifest.json?v=30',
+  './icon-192.png?v=30',
+  './icon-512.png?v=30'
 ];
 
 self.addEventListener('install', event => {
